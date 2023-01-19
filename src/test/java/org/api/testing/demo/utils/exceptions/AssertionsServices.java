06b283c742dec0159279859c7957aa4a1810d746
Camila Dimas <camiladimas18@gmail.com>
2023-01-19 00:43:38 -0500

    :triangular_flag_on_post: update exceptions

--- src/test/java/org/api/testing/demo/utils/exceptions/AssertionsServices.java
+++ src/test/java/org/api/testing/demo/utils/exceptions/AssertionsServices.java
@@ -5,7 +5,7 @@ public class AssertionsServices extends AssertionError {
     public static final String THE_STATUS_CODE_SERVICE_IS_NOT_EXPECTED = "The status code services response is not expected";
     public static final String THE_RESPONSE_TIME_SERVICE_IS_NOT_EXPECTED = "The response time is not as expected";
     public static final String THE_SCHEMA_SERVICE_IS_NOT_EXPECTED = "The schema service response is not expected";
-    public static final String THE_VALUES_SERVICE_IS_NOT_EXPECTED = "The fields and values post services response is not expected";
+    public static final String THE_VALUES_SERVICE_IS_NOT_EXPECTED = "The fields or values of the service response are not as expected.";
 
     public AssertionsServices(String message, Throwable cause) {
         super(message, cause);
